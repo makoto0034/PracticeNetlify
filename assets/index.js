bdb3977acee3b3d23711b1d38badc3fe06641cb0
makoto0034 <makotosaitou@me.com>
2023-02-06 21:02:50 +0900

    first-commit

---
+++ assets/index.js
@@ -0,0 +1,3 @@
+gsap.fromTo(".fadeDown", {opacity: 0,y:-200}, {opacity: 1,y:0, duration: 1});
+gsap.fromTo(".fadeLeft",{opacity:0,x:-200},{opacity:1,x:0,duration:1})
+gsap.fromTo(".fadeRight",{opacity:0,x:200},{opacity:1,x:0,duration:1})
